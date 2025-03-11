2d735d1424229fbe3ff6fdff55d08b913b5996ea
Paul Ehrhardt <ehrpaulhardt@gmail.com>
2025-03-11 13:25:20 +0000

    Improve secret loading

--- MovieDatatbaseCompanion/Model/Global+ApiKey.swift
+++ MovieDatatbaseCompanion/Model/Global+ApiKey.swift
@@ -11,11 +11,30 @@ import Foundation
 // MARK: - Globals -
 
 let apiKey: String = {
- //   let pathToProject = "/Users/ehd/Documents/Workspace/ehd/MovieDatatbaseCompanion"
-    let pathToProject = "/Users/ehrpaulhardt/Documents/Workspace/ehd/MovieDatatbaseCompanion"
-    let secretFileName = "/apikey.secret"
-    let fullPath = pathToProject + secretFileName
-    let fileContent = try? String(contentsOfFile: fullPath, encoding: .utf8).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    /*
+     Note: This method only works in development/debug environment where Xcode runs the app from the derived data folder.
+     If the app is running on a real device, the workspace directory won't be accessible due to sandboxing.
+     For production consider using Environment Variables, embedding the secret file inside the app bundle or fetching secrets securely via Keychain or an API.
+     */
+    let secretFileName = "apikey.secret"
+    
+    // get file path of the current file
+    let currentFilePath = URL(fileURLWithPath: #file)
+
+    // move up to the project directory and then the workspace root
+    let filePath = currentFilePath
+        .deletingLastPathComponent() // move out of the current Swift file's folder and some levels up to secrets file
+        .deletingLastPathComponent()
+        .deletingLastPathComponent()
+        .appendingPathComponent(secretFileName).path
+
+    var fileContent: String?
+    do {
+        fileContent = try String(contentsOfFile: filePath, encoding: .utf8)
+            .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    } catch {
+        print("Failed to read .secret file: \(error)")
+    }
     
     return fileContent ?? ""
 }()
